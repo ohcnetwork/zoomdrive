88b2a345a1ec3f72d974fae174ac2ad620da5637
rithviknishad <mail@rithviknishad.dev>
2023-07-17 22:45:16 +0530

    this might work?

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function downloadRecordings() {
 async function syncToGoogleDrive(files, total_size) {
   const credentials = Buffer.from(core.getInput("gsa-credentials"), "base64")
     .toString("utf-8")
-    .replace(/\\n/g, "\n");
+    .replaceAll(/\\n/g, "\n");
 
   const folderMap = JSON.parse(core.getInput("meeting-gdrive-folder-map"));
 
