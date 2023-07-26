8ef182c28bbf9317e32c420e92a26914faa25dcd
Rithvik Nishad <mail@rithviknishad.dev>
2023-07-26 15:30:28 +0000

    docs: update `GITHUB_MARKETPLACE_URL`

--- docs/src/constants.js
+++ docs/src/constants.js
@@ -1,3 +1,3 @@
 export const GITHUB_URL = 'https://github.com/coronasafe/zoomdrive'
 export const GITHUB_ORG_URL = 'https://github.com/coronasafe'
-export const GITHUB_MARKETPLACE_URL = 'https://github.com/marketplace/zoomdrive'
+export const GITHUB_MARKETPLACE_URL = 'https://github.com/marketplace/actions/zoomdrive'
